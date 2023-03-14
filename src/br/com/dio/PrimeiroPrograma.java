c1c1e9d98f8a7c9d361b91695dd87b97eedfafe6
renanzao <rclemon93@gmail.com>
2023-03-13 21:55:46 -0300

    commit add classe livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -14,8 +14,45 @@ public class PrimeiroPrograma {
         gato.setCor("Preto");
         gato.setIdade(5);
 
+        Livro livro1 = new Livro("Viver Limpo", "NA");
+
         System.out.println(gato);
+        System.out.println(livro1);
 
 
+    }
+}
+
+class Livro{
+    private String titulo;
+    private String autor;
+
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                '}';
     }
 }
